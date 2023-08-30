ea4feb40d27fd139285e3c9d2809b5f4adc93d81
jongbeeq <titivutewhangsukij@gmail.com>
2023-08-30 11:54:22 +0700

    start sidebar

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,7 @@ import { FaHome } from 'react-icons/fa';
 import Header from '../components/Header';
 import '../components/Header.scss';
 import '../index.scss';
+import { FaInbox, FaCalendar, FaCalendarAlt, FaChevronDown } from 'react-icons/fa';
 
 function App() {
   return (
@@ -10,7 +11,27 @@ function App() {
     <div className="todo__header">
       <Header/>
     </div>
-    <div className="todo__sidebar">SideBar</div>
+    <div className="todo__sidebar">
+      <aside className='sidebar'>
+        <section className='sidebar_category'>
+          <ul className='list'>
+            <li className='list__item'>
+              <FaInbox className='list__item__icon'/>
+              <p className='list__item__text'>Inbox</p>
+            </li>
+            <li className='list__item'>
+              <FaCalendar className='list__item__icon'/>
+              <p className='list__item__text'>Today</p>
+            </li>
+            <li className='list__item'>
+              <FaCalendarAlt className='list__item__icon'/>
+              <p className='list__item__text'>Next 7 days</p>
+            </li>
+          </ul>
+        </section>
+        <section className='sidebar_category'>2</section>
+      </aside>
+    </div>
     <div className="todo__content">TodoContent</div>
 </div>
   );

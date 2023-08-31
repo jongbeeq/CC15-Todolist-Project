90e1d4be1f6996d2a63b3554475c44682c4bf194
jongbeeq <titivutewhangsukij@gmail.com>
2023-08-31 15:15:15 +0700

    UI: Update TodoList Logic

--- src/components/Todo/TodoCreate.jsx
+++ src/components/Todo/TodoCreate.jsx
@@ -1,15 +1,78 @@
-import styles from './TodoCreate.module.scss';
+import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
 import { HiPlus } from 'react-icons/hi';
+import styles from './TodoCreate.module.scss';
+import TodoForm from './TodoForm';
 
+
+/*
+CC! - Condition Rendering
+- Default : Show Button & Text
+- Active : Show TodoForm
+
+Concept : true ? <AddTask/> : <TodoForm/>
+*/
+
+/*
+CC2 - EVENT HANDLING
+- เอาฟังก์ชันไปผูกติดกับ UI เพื่อให้ USER เป็นคนเรียกใช้ฟังก์ชันเอง
+- onClick : ต้อง Click ก่อน , FN ถึงจะรัน
+  - USER จะทำการคลิก
+  - BROSWER เรียกใช้ FN => Broswer กำหนด parameter ของ FN
+   handleClick(eventObject)
+*/
+
+/*
+CC3 - JS Value ไม่สามารถทำให้ React Render ได้
+ต้องใช้ state
+*/
+
+/*
+CC-4 Array Destructuring
+function myUseState() {
+  return [5,9]
+}
+let [a,b] = myUseState()
+a === 5
+b === 9
+*/
+
+
+/*
+CC5 - React State (1 ในฟังก์ชันของกลุ่ม React Hook)
+const [state,setState]= useState(initialState:any)
+  // element 1: current State
+  // element 2 : Fn สำหรับ SetState
+  // เมื่อ state เปลี่ยน FC จะ Rerender
+  // Rerender == Code ทั้งหมดใน FC จะถูกรันใหม่ 1 ครั้ง
+*/
+
+// #1 : FC = Function Component (Render)
 function TodoCreate() {
+  // HOOK FN
+  const [isOpenForm, setIsOpenForm] = useState(true)
+
+  // #2 : JS Function (Logic)
+  const handleClick = function () {
+    setIsOpenForm(!isOpenForm);
+  };
+
   return (
-    <div className={styles.todo__create}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
-      </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+    <>
+      {isOpenForm ? (
+        <TodoForm
+          textSubmit='Add Task'
+          setIsOpenForm={setIsOpenForm}
+        />
+      ) : (
+        <div className={styles.todo__create} onClick={handleClick}>
+          <div className={styles.todo__create__button}>
+            <HiPlus />
+          </div>
+          <h3 className={styles.todo__create__text}>Add Task</h3>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,21 +1,76 @@
+import { useState } from 'react';
+import { Button } from '../Common/Button/Button';
 import styles from './TodoForm.module.scss';
 
-function TodoForm() {
+/*
+props = {
+  textSubmit : string
+  setIsOpenForm : FN
+}
+*/
+
+/*
+CC1 - Form Handle
+
+-ใช้ FN ไปผูกกับ Event ชื่อ onSubmit
+-FN จะถูก Broswer เรียกใช้ (เมื่อไหร่?) โดยส่ง parameter มา 1 ตัว (event Object)
+-โดย default ทุกปุ่มใน <form> จะทหำหน้าที่ submit
+-วิธีแก้ ต้องกำหนด type ของปุ่ม
+  - type='submit'
+  - type='button'
+*/
+
+
+function TodoForm(props) {
+  const [isError, setIsError] = useState(false);
+  const [taskInput, setTaskInput] = useState("");
+  console.log(taskInput);
+
+  const handleChangeInput = function (event) {
+    if (isError) setIsError(false);
+    setTaskInput(event.target.value)
+  }
+
+  const handleSubmit = function (event) {
+    // 1.PreventDefault
+    event.preventDefault()
+
+    // 2.ต้องรู้ก่อนว่า user พิมพ์อะไร (อยู่ใน satate : taskInput)
+
+    // 3.FormValidation
+    // case1 : submit ได้
+    // case2 : submit ไม่ได้ => แสดง Error
+
+    if (taskInput.trim() === '') {
+      console.log("Error")
+      setIsError(true);
+      return;
+    }
+
+    console.log('submit');
+  };
+
+  const handleCancel = function () {
+    console.log('cancel');
+    // correctName : setIsOpenForm(false)
+    // inCorrectName : undefine(false) => บู้มมมมม!!!
+    props.setIsOpenForm(false);
+  };
+
   return (
-    <form className={styles.todo__form__container}>
+    <form className={styles.todo__form__container} onSubmit={handleSubmit}>
       {/*	Body */}
-      <input className={styles.todo__form__input} placeholder='Task Name' />
+      <input className={styles.todo__form__input} placeholder='Task Name' onChange={handleChangeInput} value={taskInput} />
 
       {/*Form Footer */}
       <div className={styles.todo__form__footer}>
-        <p className={styles.todo__error}>Title is required</p>
+        <p className={styles.todo__error}>{isError ? "Title is required" : ""}</p>
         <div className={styles.todo__form__buttons}>
-          <button>Cancel</button>
-          <button>Add Task</button>
+          <Button text="Cancel" active={false} type='button' onClick={handleCancel} />
+          <Button text={props.textSubmit} active={true} type='submit' />
         </div>
       </div>
-    </form>
-  );
+    </form>)
 }
 
 export default TodoForm;

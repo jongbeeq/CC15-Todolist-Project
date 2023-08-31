0b8152cbbbb6ad9c3542bb78ec7189750786678c
jongbeeq <titivutewhangsukij@gmail.com>
2023-08-31 09:23:17 +0700

    update P'Wee

---
+++ src/app/App.jsx
@@ -0,0 +1,30 @@
+// Dependencies
+import './App.scss';
+import AppBar from '../components/Common/AppBar/AppBar';
+import SideBar from '../components/SideBar/SideBar';
+import TodoHeader from '../components/Todo/TodoHeader';
+import TodoCreate from '../components/Todo/TodoCreate';
+import TodoLists from '../components/Todo/TodoLists';
+import { Button } from '../components/Common/Button/Button';
+
+function App() {
+  return (
+    <div className='todo'>
+      <div className='todo__header'>
+        <AppBar />
+      </div>
+      <div className='todo__sidebar'>
+        <SideBar />
+      </div>
+      <div className='todo__content'>
+        <main className='todo__container'>
+          <TodoHeader />
+          <TodoCreate />
+          <TodoLists />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Todo/TodoHeader.jsx
+++ src/components/Todo/TodoHeader.jsx
@@ -1,15 +1,15 @@
-import styles from "./TodoHeader.module.scss";
-// styles = { header : cssRule}
+import styles from './TodoHeader.module.scss';
+// styles = { header : cssRules}
 
-function TodoHeader () {
-    const today = new Date()
-    const option = {weekday: "short", day: "numeric", month: "short"}
-    return (
-        <div className={styles.header}>
-            <h1 className={styles.header__text}>Inbox</h1>
-            <span className={styles.header__date}>{today.toLocaleString('en-US',option)}</span>
-        </div>
-    );
+function TodoHeader() {
+  const today = new Date();
+  const options = { day: 'numeric', weekday: 'short', month: 'short' };
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.header__text}>Inbox</h1>
+      <span className={styles.header__date}>{today.toLocaleDateString('en-US', options)}</span>
+    </div>
+  );
 }
 
 export default TodoHeader;
